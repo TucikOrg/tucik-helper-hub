cda5368c2971905ac1bfd26968e87970c18ce053
artem <qzwsecrftb@gmail.com>
2024-12-11 21:57:39 +0300

    dev

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -5,7 +5,7 @@ export const DownloadButton = () => {
   return (
     <Button 
       className="bg-gradient-to-r from-[#38BDF8] to-[#2563EB] hover:opacity-90 transition-opacity text-white px-6 py-6 rounded-full hover-scale"
-      onClick={() => window.open("#", "_blank")}
+      onClick={() => window.open("android/tucik.apk", "_blank")}
     >
       <Download className="mr-2 h-5 w-5" />
       Download for Android
